f4498b404d46983b189b05dba18ee588d647840a
Simon Kelly <skelly@dimagi.com>
2017-05-04 14:41:02 +0200

    properly extract domain from username/domain section

--- tests/test_couch/test_couch_parsers.py
+++ tests/test_couch/test_couch_parsers.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 SIMPLE = '2015-10-31 18:32:03,963 [:mvp-pampaida] /a/mvp-pampaida/receiver/630916e49084b142c0a5a69c3a52b9b3/ PUT None d3abf611f2acdc7b4c32f7ebf4982a88 0:00:00.191515'
 WITH_CONTENT_LENGTH = '2017-05-04 12:20:18,957 [:icds-cas] /a/icds-cas/apps/download/768932dcb27f35c63cdbb830c202c727/modules-0/forms-0.xml HEAD 200 258 /commcarehq__apps/ 0:00:00.007104'
 WITH_DATABASE_NAME = '2017-05-04 12:20:21,416 [:icds-cas] /a/icds-cas/receiver/secure/768932dcb27f35c63cdbb830c202c727/ commcarehq__users GET 200 None _design/users/_view/by_username 0:00:00.004401'
+WITH_USERNAME = "2017-05-04 12:20:21,416 [123@my-dom.commcarehq.org:my-dom] /a/my-dom/phone/restore/768932dcb27f35c63cdbb830c202c727/ HEAD 200 260 /commcarehq/ 0:00:00.004076"
 BORKED = 'Borked'
 
 
@@ -53,6 +54,16 @@ class TestCouchLogParser(unittest.TestCase):
             'status_code': '200',
             'http_method': 'GET',
             'database': 'commcarehq__users'
+        })
+
+    def test_log_parsing_username(self):
+        self._test_log_parsing(WITH_USERNAME, 1493893221.0, 0.004076, {
+            'domain': 'my-dom',
+            'url': '/a/*/phone/restore/*/',
+            'couch_url': '/commcarehq/',
+            'status_code': '200',
+            'http_method': 'HEAD',
+            'database': ''
         })
 
     def test_borked_log_line(self):
