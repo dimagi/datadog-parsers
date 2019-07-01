4fa9b5d399f5ffa0351bf8528fadaf61ba6110d9
Daniel Roberts <droberts@dimagi.com>
2019-07-01 16:33:08 -0500

    Test URL_PATTERN_GROUPS format

--- tests/test_nginx/test_nginx_timings_parser.py
+++ tests/test_nginx/test_nginx_timings_parser.py
@@ -1,7 +1,8 @@
 import logging
 import unittest
 import datetime
-from nginx.timings import parse_nginx_timings, parse_nginx_apdex, parse_nginx_counter, _get_url_group, _sanitize_url
+from nginx.timings import parse_nginx_timings, parse_nginx_apdex, parse_nginx_counter, \
+    _get_url_group, _sanitize_url, URL_PATTERN_GROUPS
 from nose_parameterized import parameterized
 from parsing_utils import UnixTimestampTestMixin
 
@@ -112,6 +113,12 @@ class TestNginxTimingsParser(UnixTimestampTestMixin, unittest.TestCase):
     def test_get_url_group(self, url, expected):
         group = _get_url_group(url)
         self.assertEqual(expected, group)
+
+    def test_pattern_format(self):
+        for pattern, group_name in URL_PATTERN_GROUPS:
+            self.assert_('?P<group_name>' in pattern.pattern or group_name is not None,
+                         "If there is no capturing group (?P<group_name>) in the pattern, "
+                         "there needs to be a default: {}".format(pattern.pattern))
 
     def test_nginx_formplayer(self):
         self.assertIsNotNone(parse_nginx_timings(logging, FORMPLAYER))
