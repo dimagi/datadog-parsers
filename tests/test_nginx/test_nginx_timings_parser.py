41afb79d9e98fa1b614048e2ae162518af5c9792
Daniel Roberts <droberts@dimagi.com>
2019-07-01 16:36:37 -0500

    Add a few more nginx timings parser tests

--- tests/test_nginx/test_nginx_timings_parser.py
+++ tests/test_nginx/test_nginx_timings_parser.py
@@ -104,11 +104,16 @@ class TestNginxTimingsParser(UnixTimestampTestMixin, unittest.TestCase):
         ('/a/*/cloudcare', 'cloudcare'),
         ('/pricing/', '/pricing/'),
         ('/home/', '/home/'),
+        ('/accounts/login/', 'login'),
+        ('/accounts/login/noggin', 'other'),
         ('/a/*/phone/heartbeat/123456/', 'phone/heartbeat'),
         ('/hq/multimedia/file/CommCareAudio/123456/some-audio.mp3', 'mm/audio'),
         ('/hq/multimedia/file/CommCareVideo/123456/vid_daily_feeding.mp4', 'mm/video'),
         ('/hq/multimedia/file/CommCareImage/123456/module4_form0_en.png', 'mm/image'),
+        ('/hq/multimedia/file/', 'mm/other'),
+        ('/hq/multimedia/file/vile', 'mm/other'),
         ('/formplayer/navigate_menu', 'formplayer'),
+        ('/formplayer/', 'formplayer'),
     ])
     def test_get_url_group(self, url, expected):
         group = _get_url_group(url)
